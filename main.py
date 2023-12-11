4c4040aba112e21786228e7d5fd120c88059b8d0
tomresseguier <85993063+tomresseguier@users.noreply.github.com>
2023-12-10 21:57:22 -0500

    ajout de main.py (anciennement Lenstool_GUI.py)

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
+import argparse
+import os
+from Lenstool_GUI.fits_image import fits_image
+from Lenstool_GUI.photometry_catalog import photometry_catalog
+
+#from Lenstool_GUI.pyextract import pysex
+
+
+
+parser = argparse.ArgumentParser(prog='Lenstool_GUI',
+                                 description='A GUI for Lenstool',
+                                 epilog='#######################')
+
+
+parser.add_argument('FITS_image')
+parser.add_argument('photometry_catalog')
+#parser.add_argument('-w', '--weight_file')      # option that takes a value
+#weight_file = args.weight_file
+
+
+args = parser.parse_args()
+
+### Check that the files exist ###
+if os.path.exists( os.path.join( os.path.dirname(os.path.abspath(__file__)), args.FITS_image ) ):
+    image_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), args.FITS_image )
+elif os.path.exists( args.FITS_image ) :
+    image_path = args.FITS_image
+else :
+    print('FITS image not found')
+
+if os.path.exists( os.path.join( os.path.dirname(os.path.abspath(__file__)), args.photometry_catalog ) ):
+    photometry_catalog_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), args.photometry_catalog )
+elif os.path.exists( args.photometry_catalog ) :
+    photometry_catalog_path = args.photometry_catalog
+else :
+    print('Photometry catalog not found')
+##################################
 
 
 
 
+
+
+image_path = '/Users/Margaux/Desktop/RESEARCH/spt0615/DATA/v7-wisps/color/' + 'spt0615_color_rgb.fits'
+photometry_catalog_path = '/Users/Margaux/Desktop/RESEARCH/spt0615/DATA/v7-deblend/catalogs/' + 'spt0615_phot-eazy.csv'
+
+
+
+image = fits_image(image_path)
+photometry_catalog = photometry_catalog(photometry_catalog_path)
+
+
+### Extract galaxy shapes ###
+#pysex.run()
 
