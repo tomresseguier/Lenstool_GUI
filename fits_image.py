5c2f03f7f41ca66028d6f7686130dc8edfc2f5ba
tomresseguier <85993063+tomresseguier@users.noreply.github.com>
2023-12-10 21:10:54 -0500

    image and catalog classes
    Source extraction in image class

---
+++ fits_image.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Ellipse, Polygon, Circle
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.visualization.wcsaxes import *
+from astropy.coordinates import SkyCoord
+from astropy.visualization import ZScaleInterval, ImageNormalize
+
+import sys
+module_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(module_dir)
+
+from Lenstool_GUI.source_extraction.source_extract import source_extract
+
+
+
+class fits_image() :
+    def __init__(self, image_path) :
+        self.image_path = image_path
+        def open_image(image_path) :
+            ### OPENING THE FITS FILE ###
+            with fits.open(image_path) as hdus :
+                ### Finding the header ###
+                for hdu in hdus :
+                    if isinstance(hdu.header, fits.header.Header) :
+                        header = hdu.header
+                        break
+                wcs = WCS(hdus[0].header)
+                ### Finding the pixel scale ###
+                #if 'CD1_1' in hdus[0].header.keys() :
+                #    CD1_1 = hdus[0].header['CD1_1']
+                #    CD1_2 = hdus[0].header['CD1_2']
+                #    pix_deg_scale = np.sqrt(CD1_1**2+CD1_2**2)
+                #elif 'CDELT1' in hdus[0].header.keys() :
+                #    pix_deg_scale = abs(hdus[0].header['CDELT1'])
+                #else :
+                #    pix_deg_scale = input('Pixel scale not found in header. Please provide manually in degrees:')
+                pix_deg_scale = np.sqrt(wcs.pixel_scale_matrix[0, 0]**2+wcs.pixel_scale_matrix[0, 1]**2)
+                ### Finding the data ###
+                data = []
+                for hdu in hdus :
+                    if isinstance(hdu.data, np.ndarray) :
+                        data.append(hdu.data)
+                if len(data)<=2 :
+                    image = data[0]
+                if len(data)>=3 :
+                    data_red = data[0]
+                    data_green = data[1]
+                    data_blue = data[2]
+                    image = np.dstack((data_red, data_green, data_blue))
+            return pix_deg_scale, wcs, image
+        self.pix_deg_scale, self.wcs, self.image = open_image(image_path)
+    
+    def plot_image(self, fig=None, wcs_projection=True, units='pixel', pos=111, make_axes_labels=True) :
+        if fig is None :
+            fig = plt.figure()
+        if wcs_projection :
+            ax = fig.add_subplot(pos, projection=self.wcs)
+            ax.coords.grid(True, color='black', ls='dotted')
+        else :
+            ax = fig.add_subplot(pos)
+            if units=='pixel' or units=='pixels' or units=='image' :
+                scaling = 1
+            if units=='arcsec' :
+                scaling = pix_deg_scale*60*60
+            if units=='arcmin' :
+                scaling = pix_deg_scale*60
+        if make_axes_labels and wcs_projection :
+            ax.coords[0].set_axislabel('Right ascension')
+            ax.coords[1].set_axislabel('Declination')
+        elif make_axes_labels and not wcs_projection :
+            ax.set_xlabel('x (' + units + ')')
+            ax.set_ylabel('y (' + units + ')')
+        elif not make_axes_labels and wcs_projection :
+            ax.coords[0].set_axislabel(' ')
+            ax.coords[1].set_axislabel(' ')
+        else :
+            ax.set_xlabel(' ')
+            ax.set_ylabel(' ')
+        if wcs_projection :
+            ax.imshow(self.image, origin="lower")
+        if not wcs_projection :
+            ax.imshow(self.image, origin='lower', extent=[0, self.image.shape[1]*scaling, 0, self.image.shape[0]*scaling])
+        #ax.figure.tight_layout()
+        return fig, ax        
+    
+    def extract_sources(self) :
+        self.sources = source_extract(self.image_path, weight_path=None, zero_point=None,
+                       out_dir='PWD', outfile_name='SExtractor_cat', return_sources=True)
+        return self.sources
+
+
+
+
+    
+    
+    
---
+++ source_extraction/source_extract.py
@@ -0,0 +1,133 @@
+"""
+source_extract.py from David Harvey's pyRRG
+"""
+
+#from pyextract import pysex
+from Lenstool_GUI.source_extraction.pysex import pysex
+from astropy.io import fits
+
+import numpy as np
+import os as os
+from Lenstool_GUI.source_extraction.match_cat import run_match
+from numpy.lib.recfunctions import append_fields as append_rec
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+def source_extract( image_path, weight_path=None, zero_point=None,
+                    out_dir='PWD', outfile_name=None, return_sources=True) :
+    '''
+    Given that source extration is a difficult thing for the photometry
+    I will use the Mathilde mehtod, in Python to do a wrapper
+    for source_extractor.
+    
+    The process is the same as in mathilde_setract.pro
+    THe weight file is optional however should be used.
+    zero_point is the zero point of the iamge, if not used, it
+    will guess it from the header of the file and assume it is ACS
+
+    This will run two runs of sextractor, cold and hot, where cold only finds
+    big things and hot finds small things
+
+    Then it combines the two catalogues
+    
+    '''
+    
+    config_dir = os.path.join(module_dir, 'Lenstool_GUI/source_extraction/SExtractor_config/from_pyRRG/')
+    check_sex_files(config_dir)
+    
+    if out_dir == 'PWD':
+        out_dir = os.getcwd()
+    
+    header = fits.open( image_path )[0].header
+    findPhot = np.array(['PHOTFLAM' in key for key in header.keys()])
+    if np.all(findPhot == False) :
+        header = fits.open( image_path )[1].header
+    
+    ### FIX THE PHOTOMETRY HERE!!! AND ADD WEIGHT FILE OPTION ###
+    zero_point = 0. #acs_zero_point(header)
+        
+    conf_args = {'MAG_ZEROPOINT': zero_point,
+                 'WEIGHT_TYPE': 'NONE',
+                 'PARAMETERS_NAME': config_dir + '/rrg.param',
+                 'STARNNW_NAME': config_dir + '/default.nnw',
+                 'FILTER_NAME': config_dir + '/gauss_5.0_9x9.conv'}
+    
+    
+    #F## COLD RUN ###
+    cold_conf = config_dir + '/HFF_cold.param'
+    cold_sources = pysex.run( image_path, \
+                              conf_file=cold_conf, \
+                              conf_args=conf_args, \
+                              param_file=config_dir+'/rrg.param')
+    
+    cold_sources = append_fits_field( cold_sources, 'RA', cold_sources['X_WORLD'])
+    cold_sources = append_fits_field( cold_sources, 'DEC', cold_sources['Y_WORLD'])
+    
+    
+
+    #Second hot 
+    hot_conf = config_dir+'/HFF_hot.param'
+        
+    hot_sources = pysex.run( image_path, \
+                               conf_file=hot_conf, \
+                               conf_args=conf_args, \
+                               params=config_dir+'/rrg.param')
+
+
+    hot_sources = append_fits_field( hot_sources, 'RA', hot_sources['X_WORLD'])
+    hot_sources = append_fits_field( hot_sources, 'DEC', hot_sources['Y_WORLD'])
+    
+    #The NYMBER is a weird thing
+    
+    hot_sources['NUMBER'] = np.arange( len(hot_sources['NUMBER'])) +1
+    cold_sources['NUMBER'] = np.arange( len(cold_sources['NUMBER'])) +1
+    
+    fits.writeto( 'cold_sources.fits', cold_sources, clobber=True )
+    fits.writeto( 'hot_sources.fits', hot_sources, clobber=True )
+
+
+    
+    matched_sources = run_match( 'cold_sources.fits',
+                                'hot_sources.fits')
+
+    for iField in hot_sources.columns.names:
+        hot_sources[iField][ matched_sources[1].data['NUMBER_2'] -1 ] = \
+          cold_sources[iField][ matched_sources[1].data['NUMBER_1'] - 1]
+
+    #Need to retain numbering for bookeepin purposes
+    hot_sources['NUMBER'] = np.arange(len(hot_sources))
+  
+    if outfile_name is not None:
+        fits.writeto( out_dir + '/' + outfile_name, hot_sources)
+    if return_sources:
+        return hot_sources
+    
+    
+def acs_zero_point( header ):
+    zpt = -2.5*np.log10(header['PHOTFLAM']) + header['PHOTZPT'] - 5.0*np.log10(header['PHOTPLAM'])+18.6921
+    return zpt
+
+def check_sex_files( config_dir ):
+    #Check all the sex files to see if they exist
+    if (not os.path.isfile(config_dir+'/HFF_hot.param')):
+        raise ValueError('HFF_hot.param not found')
+    if (not os.path.isfile(config_dir+'/HFF_cold.param')):
+        raise ValueError('HFF_cold.param not found')
+    if (not os.path.isfile(config_dir+'/rrg.param')):
+        raise ValueError('rrg.param not found')
+
+
+    
+def append_fits_field( fits_array, name, array, format='D'):
+    
+    cols = [] 
+    cols.append(
+        fits.Column(name=name, format=format, array=array ))
+                          
+    orig_cols = fits_array.columns
+    new_cols = fits.ColDefs(cols)
+    new_fits = fits.BinTableHDU.from_columns(orig_cols + new_cols)
+    return new_fits.data
